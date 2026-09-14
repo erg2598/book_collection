80ef611f9e660fcba331a9045aa29a4db3e3c1c7
Eli Goodrich <erg2598@tamu.edu>
2026-09-14 07:06:10 +0000

    created main page and started making show page

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -1,16 +1,54 @@
 class BookController < ApplicationController
   def home
-  end
-
-  def new
-  end
-
-  def update
+    @books = Book.order(:id)
   end
 
   def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if book.save
+      redirect_to(book_path)
+    else
+      render('new')
+    end
+
+  end
+
+  def edit
+    @book = book.find(params[:id])
+  end
+
+  def update
+    @book = book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to(book_path(@book))
+    else
+      render('edit')
+    end
   end
 
   def delete
+    @book = Book.find(params[:id])
   end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @Book.destroy
+    redirect_to(tasks_path)
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title)
+  end
+
+  
 end
+
